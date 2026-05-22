1b2d3f676e5cf2f30b010d1a55914ae6266e1edf
swdee <shanon@bokumail.com>
2026-05-22 22:39:02 +1200

    added parallel matmul back in temporarily whilst working yolov5 code

--- postprocess/matmul_neon.go
+++ postprocess/matmul_neon.go
@@ -36,6 +36,8 @@ void matmul_mask_f32_neon(
 import "C"
 
 import (
+	"runtime"
+	"sync"
 	"unsafe"
 )
 
@@ -79,4 +81,53 @@ func matmulUint8(
 		C.int(protoC),
 		C.int(area),
 	)
+}
+
+func matmulUint8Parallel(
+	data *strideData,
+	boxesNum int,
+	protoC int,
+	protoH int,
+	protoW int,
+	out []uint8,
+) {
+	area := protoH * protoW
+	if boxesNum <= 0 || protoC <= 0 || area <= 0 {
+		return
+	}
+
+	workers := runtime.GOMAXPROCS(0)
+	if workers > boxesNum {
+		workers = boxesNum
+	}
+	if workers < 1 {
+		workers = 1
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(workers)
+
+	for worker := 0; worker < workers; worker++ {
+		startBox := boxesNum * worker / workers
+		endBox := boxesNum * (worker + 1) / workers
+		count := endBox - startBox
+
+		go func(startBox int, count int) {
+			defer wg.Done()
+
+			coeffOffset := startBox * protoC
+			outOffset := startBox * area
+
+			C.matmul_mask_f32_neon(
+				(*C.float)(unsafe.Pointer(&data.proto[0])),
+				(*C.float)(unsafe.Pointer(&data.filterSegmentsByNMS[coeffOffset])),
+				(*C.uint8_t)(unsafe.Pointer(&out[outOffset])),
+				C.int(count),
+				C.int(protoC),
+				C.int(area),
+			)
+		}(startBox, count)
+	}
+
+	wg.Wait()
 }
